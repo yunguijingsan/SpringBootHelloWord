77f711e0aa39b801af0f338701d6dd887066e38a
yunguijingsan <396215280@qq.com>
2016-11-22 17:48:02 +0800

    second
    second

--- src/main/java/cn/lcf/core/spring/CustomJsonHttpMessageConverter.java
+++ src/main/java/cn/lcf/core/spring/CustomJsonHttpMessageConverter.java
@@ -5,15 +5,30 @@ import java.io.IOException;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
 
-public class CustomJsonHttpMessageConverter extends MappingJackson2HttpMessageConverter{
+import cn.lcf.core.exception.ResponseResult;
 
+@Component
+public class CustomJsonHttpMessageConverter extends
+		MappingJackson2HttpMessageConverter {
+
+	
 	@Override
 	protected void writeInternal(Object obj, HttpOutputMessage httpOutputMessage)
 			throws IOException, HttpMessageNotWritableException {
-		super.writeInternal(obj, httpOutputMessage);
+		if(obj instanceof ResponseResult){
+			ResponseResult<?> responseResult = (ResponseResult<?>) obj;
+			this.writeResponseResult(responseResult);
+		}else{
+			super.writeInternal(obj, httpOutputMessage);
+		}
+	
 	}
 
+	private void writeResponseResult(ResponseResult<?> responseResult) {
 
+	   
+	}
 
 }

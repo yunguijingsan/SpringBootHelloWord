1fb618d57513011c3a1efb052468344ab78018ba
yunguijingsan <396215280@qq.com>
2016-12-07 17:45:43 +0800

    jdbc链接数
    链接数

--- src/main/java/cn/lcf/core/filter/WebMvcConfig.java
+++ src/main/java/cn/lcf/core/filter/WebMvcConfig.java
@@ -14,6 +14,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
   public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(signatureInterceptor).addPathPatterns("/application/*").excludePathPatterns(".html");
+//		registry.addInterceptor(signatureInterceptor).addPathPatterns("/application/*").excludePathPatterns(".html");
     }
 }
--- src/main/java/cn/lcf/core/servlet/DruidStatViewServlet.java
+++ src/main/java/cn/lcf/core/servlet/DruidStatViewServlet.java
@@ -8,8 +8,8 @@ import com.alibaba.druid.support.http.StatViewServlet;
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/druid/*", 
     initParams={
-            @WebInitParam(name="loginUsername",value="lcf"),// 用户名
-            @WebInitParam(name="loginPassword",value="lcf"),// 密码
+//            @WebInitParam(name="loginUsername",value="lcf"),// 用户名
+//            @WebInitParam(name="loginPassword",value="lcf"),// 密码
             @WebInitParam(name="resetEnable",value="false")// 禁用HTML页面上的“Reset All”功能
     })
 public class DruidStatViewServlet extends StatViewServlet {

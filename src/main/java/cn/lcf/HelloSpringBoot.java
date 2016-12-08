3b2e60da5cea28df75980f22f425884ef637e8fc
yunguijingsan <396215280@qq.com>
2016-12-08 17:10:51 +0800

    mq
    mq

--- src/main/java/cn/lcf/HelloSpringBoot.java
+++ src/main/java/cn/lcf/HelloSpringBoot.java
@@ -1,14 +1,23 @@
 package cn.lcf;
 
+import javax.jms.Queue;
+
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @ServletComponentScan
 @Configuration("classpath*:druid.xml")
 public class HelloSpringBoot {
+	@Bean
+	public Queue queue() {
+		return new ActiveMQQueue("sample.queue");
+	}
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HelloSpringBoot.class, args);
 	}
---
+++ src/main/java/cn/lcf/core/queue/Consumer.java
@@ -0,0 +1,14 @@
+package cn.lcf.core.queue;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+	@JmsListener(destination = "sample.queue")
+	public void receiveQueue(String text) {
+		System.out.println(text);
+	}
+
+}
